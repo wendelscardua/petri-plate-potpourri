23aa11d9847886713ba35aa25cc9ae4afe9a34af
Wendel Scardua <wendelscardua@gmail.com>
2024-10-05 20:13:45 -0300

    Move lens up and down loading and unloading tiles

--- src/lens.cpp
+++ src/lens.cpp
@@ -2,9 +2,12 @@
 #include "banked-metasprites.hpp"
 #include "input.hpp"
 #include "metasprites.hpp"
+#include <nesdoug.h>
 #include <neslib.h>
 
-Lens::Lens(u8 x, u8 y, Input& input, u8 (&mirror)[]) : mirror(mirror), x(x, 0), y(y, 0), x_speed(0), y_speed(0), input(input), horizontal_direction(Right), vertical_direction(Down) {}
+Lens::Lens(u8 x, u8 y, Input &input, u8 (&mirror)[])
+    : mirror(mirror), x(x, 0), y(y, 0), x_speed(0), y_speed(0), input(input),
+      horizontal_direction(Right), vertical_direction(Down) {}
 
 void Lens::draw_sprite() {
   banked_oam_meta_spr_horizontal(x.as_i(), y.as_i() - 1, UI_BANK, UI::lens);
@@ -94,5 +97,51 @@ void Lens::update() {
     if (y_speed > 0) {
       y_speed = 0;
     }
+  }
+
+  const u8 blank_strip[] = {3, 3, 3, 3, 3, 3};
+  u8 new_strip[6];
+
+  switch (vertical_direction) {
+  case Up: {
+    u8 column = x.as_i() / 8 - 3;
+    if (x.as_i() & 0b111) { // round up to the next tile
+      column++;
+    }
+    // delete row below
+    u8 old_row = y.as_i() / 8 + 3;
+    if (y.as_i() & 0b111) { // round up to the next tile
+      old_row++;
+    }
+    multi_vram_buffer_horz(blank_strip, 6, NTADR_A(column, old_row));
+    // draw row above
+    u8 new_row = y.as_i() / 8 - 3;
+    multi_vram_buffer_horz(mirror + NTADR_A(column, new_row) -
+                               NTADR_A(0, 4),
+                           6, NTADR_A(column, new_row));
+  } break;
+  case Down: {
+    u8 column = x.as_i() / 8 - 3;
+    if (x.as_i() & 0b111) { // round up to the next tile
+      column++;
+    }
+    // delete row above
+    u8 old_row = y.as_i() / 8 - 4;
+    multi_vram_buffer_horz(blank_strip, 6, NTADR_A(column, old_row));
+    // draw row below
+    u8 new_row = y.as_i() / 8 + 2;
+    if (y.as_i() & 0b111) { // round up to the next tile
+      new_row++;
+    }
+    multi_vram_buffer_horz(mirror + NTADR_A(column, new_row) -
+                               NTADR_A(0, 4),
+                           6, NTADR_A(column, new_row));
+  } break;
+  }
+
+  switch (horizontal_direction) {
+  case Left:
+  case Right:
+    break;
   }
 }
