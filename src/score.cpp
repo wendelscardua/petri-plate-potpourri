281c87e85cd4848bf3c9698b59a0e0925f50b531
Wendel Scardua <wendelscardua@gmail.com>
2024-10-07 00:20:54 -0300

    Display score texts delayed

--- src/score.cpp
+++ src/score.cpp
@@ -1,5 +1,6 @@
 #include "score.hpp"
 #include "assets-loader.hpp"
+#include "charset.hpp"
 #include <nesdoug.h>
 #include <neslib.h>
 
@@ -16,8 +17,30 @@ Score::~Score() {
 }
 
 void Score::run() {
+  u16 delay_counter = 0;
   while (true) {
     rand16();
     ppu_wait_nmi();
+
+    switch (delay_counter) {
+    case 48:
+      multi_vram_buffer_horz("Performance "_ts, 12, NTADR_A(6, 10));
+      multi_vram_buffer_horz(" report:    "_ts, 12, NTADR_A(6, 11));
+      break;
+    case 96:
+      multi_vram_buffer_horz(" Plates     "_ts, 12, NTADR_A(6, 13));
+      multi_vram_buffer_horz(" cleared:   "_ts, 12, NTADR_A(6, 14));
+      break;
+    case 144:
+      // TODO display current score
+      break;
+    case 192:
+      multi_vram_buffer_horz(" Best:      "_ts, 12, NTADR_A(6, 16));
+      break;
+    case 240:
+      // TODO display best score
+      break;
+    }
+    delay_counter++;
   }
 }
